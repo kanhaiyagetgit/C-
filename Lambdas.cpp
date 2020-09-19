8c14d649a85aab3715bc1569081da32a342f163c
kanhaiyagetgit <kanhaiyas56@gmail.com>
2020-09-19 13:43:14 +0530

    Lambdas and string sorting using vector added

---
+++ Lambdas.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    vector<string> names{ "Kishan","Kanhaiya","Guro","papa","Mommy"};
+    sort(begin(names),end(names),[](string const& a,string const& b)
+    {
+        return a.length() < b.length();//To sort according to length as well
+    }
+    );
+    for (auto x: names)
+    cout<<x<<endl;
+}
