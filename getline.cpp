ea6e293a8cf5d1167c1adc897facd295bc705ccf
kanhaiyagetgit <kanhaiyas56@gmail.com>
2020-09-19 16:51:32 +0530

    getline added

---
+++ getline.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//#include<vector>
+#include<string>
+//#include<algorithm>
+using namespace std;
+int main()
+{
+    string s;
+    cout<<"Enter a sentence"<<endl;
+    getline(cin,s);
+    cout<<"Hey man ! "<<s; 
+}
