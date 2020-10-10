305a169e3bdb71e9daa3eb06205f2f18df80e64a
kanhaiyagetgit <kanhaiyas56@gmail.com>
2020-10-10 19:25:28 +0530

    added more

--- 2d_array.cpp
+++ 2d_array.cpp
@@ -4,6 +4,11 @@ int main()
 {
     int a[2][2]={1,2,3,4};
     int *p[]={(int *)a,(int *)a+1,(int *)a+2};
-    for(auto x : p)
-    cout<<*x<<endl;
+    for (int i=0;i<2;i++)
+    {
+        for (int j=0;j<2;j++)
+        cout<<*(*(p+i)+j)<<" "<<*(*(j+p)+i)<<" "<<*(*(i+p)+j)<<" "<<*(*(p+j)+i)<<endl;
+
+    }
+
 }
