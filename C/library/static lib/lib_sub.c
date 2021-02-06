2ad17e11fb384cee20b677003cd848819ff70ac1
kanhaiyagetgit <kanhaiyas56@gmail.com>
2021-02-06 13:38:58 +0530

    lib

---
+++ C/library/static lib/lib_sub.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void sub(int a, int b)
+{
+    printf("Sub is  : %d",a-b);
+}
---
+++ C/library/static lib/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include"calc.h"
+void main()
+{
+    int a=3,b=5;
+    add(a,b);
+    sub(a,b);
+}
